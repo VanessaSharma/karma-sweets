099175f9a38351a18314cdf85f19d86b11936986
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-11-02 13:26:03 -0700

    Refactor to include new product-detail and product-tile components.

--- app/components/product-tile.js
+++ app/components/product-tile.js
@@ -1,14 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions: {
-    update(product, params) {
-      this.sendAction('update', product, params);
-    },
-    delete(product) {
-      if(confirm('Are you sure want to delete this product?🍰')) {
-        this.sendAction('destroyProduct', product);
-      }
-    }
-  }
 });
